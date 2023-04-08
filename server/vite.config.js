842b6eea98b92d357a637604c864a163c09670d6
Martin Alstrup <martin@rcz.dk>
2023-04-08 20:52:59 +0200

    Added env var to configure build dir

--- server/vite.config.js
+++ server/vite.config.js
@@ -2,16 +2,23 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
   if (mode === 'development') {
     return {
       build: {
         emptyOutDir: false,
+        outDir: process.env.VITE_BUILD_DIR
         copyPublicDir: false
       },
       plugins: [ vue() ],
     }
   } else {
     return {
+      build: {
+        emptyOutDir: true,
+        outDir: process.env.VITE_BUILD_DIR
+      },
       plugins: [ vue() ],
     }
   }
