6abc1c109bb1029e2e9d837cb89e82f0081a6956
Martin Alstrup <martin@rcz.dk>
2023-07-31 19:12:19 +0200

    Minor upgrades to tcptester

--- server/src/tools/tcptester.js
+++ server/src/tools/tcptester.js
@@ -23,8 +23,17 @@ client.connect(1337, '127.0.0.1', () => {
 	rl.prompt()
 })
 
+client.on('timeout', () => {
+	console.log('Connection timed out')
+	socket.end();
+})
+
 client.on('disconnect', function() {
 	console.log('Connection closed')
+	client.connect(1337, '127.0.0.1', () => {
+		console.log('Reconnected')
+		rl.prompt()
+	})
 })
 
 let encode = function(cmd, name1 = '', name2 = '') {
