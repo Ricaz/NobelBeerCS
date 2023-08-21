ccca9dfb40828eee9cc8e72480c0ef1404555567
Martin Alstrup <martin@rcz.dk>
2023-08-21 19:05:56 +0200

    Split components into separate files

--- server/src/webapp.js
+++ server/src/webapp.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App.vue'
+import Scoreboard from './components/Scoreboard.vue'
+import ScoreboardRow from './components/ScoreboardRow.vue'
 
-const app = createApp(App).mount('#app')
+const app = createApp(App)
+	.component('Scoreboard', Scoreboard)
+	.component('ScoreboardRow', ScoreboardRow)
+	.mount('#app')
 
 import 'bootstrap/dist/js/bootstrap.js'
