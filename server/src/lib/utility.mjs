9f21c9d77fd5c1997f2b012f998fbb957f64f660
Martin Alstrup <martin@rcz.dk>
2025-03-06 14:51:05 +0100

    Indentation

--- server/src/lib/utility.mjs
+++ server/src/lib/utility.mjs
@@ -1,17 +1,17 @@
 function tcp(msg) {
-  console.log('[TCP]   ' + msg)
+	console.log('[TCP]   ' + msg)
 }
 
 function ws(msg) {
-  console.log('[WS]    ' + msg)
+	console.log('[WS]    ' + msg)
 }
 
 function http(msg) {
-  console.log('[HTTP]  ' + msg)
+	console.log('[HTTP]  ' + msg)
 }
 
 function score(msg) {
-  console.log('[SCORE] ' + msg)
+	console.log('[SCORE] ' + msg)
 }
 
 export { tcp, ws, http, score }
